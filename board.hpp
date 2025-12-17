eaa68bb18732f3ff111fd4d8691f5e53820dd143
TakutoMorigaki <taku109ttaakkuu@gmail.com>
2025-12-18 00:48:32 +0900

    boardの修正

--- board.hpp
+++ board.hpp
@@ -18,41 +18,44 @@ enum Action {
     ACT_RIGHT = 0,
     ACT_LEFT  = 1,
     ACT_UP    = 2,
-    ACT_DOWN  = 3
+    ACT_DOWN  = 3,
 };
 
 // ボードの初期化
-void init_board(board_2048 *board);
+void init_board(board_2048 &board);
 
 // 一つのマスにおいて上下左右に動けるかを判定する
-void CanMove_grid(board_2048 *board);
+void CanMove_grid(board_2048 &board);
 // 右に動けるか判定(true:動ける, false:動けない)
-bool CanMove_R(board_2048 *board);
+bool CanMove_R(board_2048 &board);
 // 左に動けるか判定(true:動ける, false:動けない)
-bool CanMove_L(board_2048 *board);
+bool CanMove_L(board_2048 &board);
 // 上に動けるか判定(true:動ける, false:動けない)
-bool CanMove_U(board_2048 *board);
+bool CanMove_U(board_2048 &board);
 // 下に動けるか判定(true:動ける, false:動けない)
-bool CanMove_D(board_2048 *board);
+bool CanMove_D(board_2048 &board);
 
 // ボードを右に動かす
-void Move_R(board_2048 *board);
+void Move_R(board_2048 &board);
 // ボードを左に動かす
-void Move_L(board_2048 *board);
+void Move_L(board_2048 &board);
 // ボードを上に動かす
-void Move_U(board_2048 *board);
+void Move_U(board_2048 &board);
 // ボードを下に動かす
-void Move_D(board_2048 *board);
+void Move_D(board_2048 &board);
 
+// AIのactionで盤面が動いたかを判定
+bool apply_action(board_2048 &board, Action action);
 // AIを一手進める
-bool step(board_2048 *board, Action action);
+bool step(board_2048 &board, Action action);
 // 状態をAI用に変換
-void get_state(const board_2048 *board, float state[16]);
-// AIの着手方向
-int ai_direc();
+void get_state(const board_2048 &board, float state[16]);
+// ランダムAIの着手方向
+int ai_direc_random();
 
 // 新しく数字をボードに追加する
-void Pop_value(board_2048 *board);
+void Pop_value(board_2048 &board);
+
 
 // スコアの計算
-int calc_score(board_2048 *board);
+int calc_score(board_2048 &board);
